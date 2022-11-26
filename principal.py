6d8c3966044516edff0630d574396939cd8d8caa
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-26 15:46:15 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,11 +1,22 @@
-import streamlit as st
-import pandas as pd #manipulacao 
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+#Copiar o endereço do seu google drive onde esta rodando o projeto e o arquivo 'diabetes.csv' esta dentro.
+dados = pd.read_csv("diabetes.csv")vírgula
+
+#planilha.info()
+nomesColunas = planilha.columns.to_list() #Nomes recebem todas as colunas e são colocadas numa lista
+print("\n NOME DAS COLUNAS: \n",nomesColunas[:8])
 
 
 ###################importando os dados do csv ########################
-dados = pd.read_csv("diabetes.csv")
 
-#coleta os nomes das colunas
+
+'''#coleta os nomes das colunas
 nomes_colunas = dados.columns.to_list()
 tamanho = len(nomes_colunas)
 nomes_colunas = nomes_colunas[:tamanho-1]
