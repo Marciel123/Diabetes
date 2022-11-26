76ec39e530d0514e7714f01ecb7c245072a934ab
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-26 15:41:11 -0300

    Create principal.py

---
+++ principal.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd #manipulacao 
+
+
+###################importando os dados do csv ########################
+dados = pd.read_csv("diabetes.csv")
+
+#coleta os nomes das colunas
+nomes_colunas = dados.columns.to_list()
+tamanho = len(nomes_colunas)
+nomes_colunas = nomes_colunas[:tamanho-1]
+features = dados[nomes_colunas]
+classes = dados['Outcome']
+features.shape,classes.shape
+st.write(nomes_colunas)
+#dividir entre treino e teste
+
+from sklearn.model_selection import train_test_split
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,classes,test_size=0.3,random_state=2)
+from sklearn.tree import DecisionTreeClassifier
+arvore = DecisionTreeClassifier()
